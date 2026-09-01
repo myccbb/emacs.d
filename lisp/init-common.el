efbaa5afe3a7b85d056f3b1ab3597b62307e267b
liuyang <liuyangmy35@gmail.com>
2026-09-01 15:52:20 +0800

    recover evil-mode config

--- lisp/init-common.el
+++ lisp/init-common.el
@@ -128,12 +128,12 @@
   :config
   (setq-default org-startup-truncated nil)
   (setq-default org-startup-folded nil)
-  ;;(add-hook 'org-mode-hook
-  ;;          (lambda ()
-  ;;            (progn
-  ;;              (setq-local evil-auto-indent nil)
-  ;;              (define-key evil-normal-state-map (kbd "TAB") 'org-cycle)
-  ;;              )))
+  (add-hook 'org-mode-hook
+            (lambda ()
+              (progn
+                (setq-local evil-auto-indent nil)
+                (define-key evil-normal-state-map (kbd "TAB") 'org-cycle)
+                )))
   )
 
 (provide 'init-common)
